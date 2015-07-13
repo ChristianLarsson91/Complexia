dc3133d0c44fdd1a08dc55764baf7a643d1c3ef7
ChristianLarsson91 <chrl@student.chalmers.se>
2015-07-13 11:47:43 +0200

    Initial commit

---
+++ android/src/com/jc/complexia/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jc.complexia.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jc.complexia.Complexia;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Complexia(), config);
+	}
+}
